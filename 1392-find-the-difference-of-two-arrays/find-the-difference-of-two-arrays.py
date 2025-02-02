8fd650cf477bbde27c4d2535ced4e99d78e554d9
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-02-01 18:28:20 -0800

    Time: 7 ms (70.84%) | Memory: 18 MB (53.99%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -3,7 +3,4 @@ class Solution:
         set1 = set(nums1)
         set2 = set(nums2)
 
-        list1 = list(set1.difference(set2))
-        list2 = list(set2.difference(set1))
-
-        return [list1, list2]
+        return [list(set1.difference(set2)), list(set2.difference(set1))]
