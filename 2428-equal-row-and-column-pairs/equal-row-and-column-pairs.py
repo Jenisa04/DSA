340bc30d63963e99adafa9ac65a12e99bb2789b5
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-02-02 04:05:18 -0800

    Time: 160 ms (17.66%) | Memory: 21.8 MB (87.76%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -1,6 +1,5 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        # columns = []
         column = []
         ct = 0
         for col in range(len(grid)):
@@ -11,13 +10,5 @@ class Solution:
             for allRows in grid:
                 if allRows == column:
                     ct+=1
-            # columns.append(column)
             column = []
-        # print(ct)
-        # now iterate through all the lists in grid and check if it matches with a list in columns
-        # count = 0
-        # for i in range(len(grid)):
-        #     for j in range(len(columns)):
-        #         if grid[i] == columns[j]:
-        #             count += 1
         return ct
