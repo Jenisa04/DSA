b6c23e728b57009aa428cc0153aec34f989da36c
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-02-02 17:41:41 -0800

    Time: 0 ms (100.00%) | Memory: 18.2 MB (17.84%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,18 +1,7 @@
 class Solution:
+    # Jenisa
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # res = []
-        # traverse over nums1 first
-        # for i in range(len(nums1)):
-        #     temp = -1
-        #     for j in range(len(nums2)):
-        #         if nums1[i] == nums2[j]:                    
-        #             for k in range(j, len(nums2)):
-        #                 if nums2[k] > nums2[j]:
-        #                     temp = nums2[k]
-        #                     break
-        #     res.append(temp)
-
-        # Approach 2:
+        # Approach:
         # create empty hashmap that stores the greater than value for each element in nums2 else -1
         # traverse over nums2
         res = []
