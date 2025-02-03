8b47e38d0f1ed734205d22bfd36386b2a99107d7
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-02-02 23:35:09 -0800

    Time: 32 ms (32.10%) | Memory: 27 MB (10.56%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,9 +3,10 @@ class Solution:
         rightProd = [1] * len(nums)
         leftProd = [1]
         
-        if len(nums) == 2:
-            nums[0], nums[1] = nums[1], nums[0]
-            return nums
+        # if len(nums) == 2:
+        #     nums[0], nums[1] = nums[1], nums[0]
+        #     return nums
+
         for i in range(len(nums)-1):
             leftProd.append(leftProd[i] * nums[i])
         for j in range(len(nums)-2, -1, -1):
