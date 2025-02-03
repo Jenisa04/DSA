25c799575d4978289330f20aa898609b639772fa
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-02-02 17:39:00 -0800

    Time: 4 ms (32.85%) | Memory: 17.9 MB (54.70%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,15 +1,29 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res = []
+        # res = []
         # traverse over nums1 first
-        for i in range(len(nums1)):
-            temp = -1
-            for j in range(len(nums2)):
-                if nums1[i] == nums2[j]:                    
-                    for k in range(j, len(nums2)):
-                        if nums2[k] > nums2[j]:
-                            temp = nums2[k]
-                            break
-            res.append(temp)
+        # for i in range(len(nums1)):
+        #     temp = -1
+        #     for j in range(len(nums2)):
+        #         if nums1[i] == nums2[j]:                    
+        #             for k in range(j, len(nums2)):
+        #                 if nums2[k] > nums2[j]:
+        #                     temp = nums2[k]
+        #                     break
+        #     res.append(temp)
 
+        # Approach 2:
+        # create empty hashmap that stores the greater than value for each element in nums2 else -1
+        # traverse over nums2
+        res = []
+        ctr = dict()
+        for i in range(len(nums2)):
+            ctr[nums2[i]] = -1
+            for j in range(i, len(nums2)):
+                if nums2[j] > nums2[i]:
+                    ctr[nums2[i]] = nums2[j]
+                    break
+        # now traverse over nums1 and find the next largest element from hashmap
+        for idx, val in enumerate(nums1):
+            res.append(ctr[val])
         return res
