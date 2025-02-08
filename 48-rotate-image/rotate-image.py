0b17ea2772aaadd92dfe22e677b56216cc9bbd30
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-02-08 14:13:47 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.17%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -1,11 +1,12 @@
-import copy
 class Solution:
+    # Jenisa
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        nums = copy.deepcopy(matrix)
-        numCols = numRows = len(nums)
-        for r in range(numRows):
-            for c in range(numCols):
-                matrix[c][numRows-r-1] = nums[r][c]
+        # Approach 2: Transpose first and then reverse
+        for r in range(len(matrix)):
+            for c in range(r+1, len(matrix)):
+                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+        for row in matrix:
+            row.reverse()
