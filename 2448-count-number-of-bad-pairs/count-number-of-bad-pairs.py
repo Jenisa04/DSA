8ef71bc4606babce3716535590a019fe2a01ef2b
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-02-08 18:40:54 -0800

    Time: 79 ms (59.05%) | Memory: 38.8 MB (59.29%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -1,22 +1,31 @@
 import math
 class Solution:
-    # Jenisa
     def countBadPairs(self, nums: List[int]) -> int:
         # j - i != nums[j] - nums[i] ==> 
         # ==> nums[i] - i != nums[j] - j
-        count = []
-        diff = dict()
+        # count = []
+        # diff = dict()
     
-        for i in range(len(nums)):
-            if nums[i] - i not in diff:
-                diff[nums[i] - i] = 1
-            else:
-                diff[nums[i] - i] += 1
-                if nums[i] -i not in count:
-                    count.append(nums[i] - i)
+        # for i in range(len(nums)):
+        #     if nums[i] - i not in diff:
+        #         diff[nums[i] - i] = 1
+        #     else:
+        #         diff[nums[i] - i] += 1
+        #         if nums[i] -i not in count:
+        #             count.append(nums[i] - i)
 
-        totalPairs = (len(nums) * (len(nums) - 1)) // 2
-        for j in count:
-            totalPairs -= math.comb(diff[j],2)
+        # totalPairs = (len(nums) * (len(nums) - 1)) // 2
+        # for j in count:
+        #     totalPairs -= math.comb(diff[j],2)
         
-        return totalPairs
+        # return totalPairs
+        freq = {}
+        good_pairs = 0
+
+        for i, num in enumerate(nums):
+            key = num - i
+            good_pairs += freq.get(key, 0)
+            freq[key] = freq.get(key, 0) + 1
+
+        n = len(nums)
+        return (n * (n - 1)) // 2 - good_pairs
