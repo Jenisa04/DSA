a6b10e59c4e7ed67abd02594c4739be1d9e59b81
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-02-08 18:27:47 -0800

    Time: 1725 ms (5.47%) | Memory: 38.5 MB (91.43%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -6,13 +6,40 @@ class Solution:
         # ==> nums[i] - i != nums[j] - j
         count = []
         diff = dict()
-        for i in range(len(nums)):
+        i = 0
+        j = len(nums)-1
+        while i <= j:
+            if i == j:
+                if nums[i] - i not in diff:
+                    diff[nums[i] - i] = 1
+                else:
+                    diff[nums[i] - i] += 1
+                    if nums[i] -i not in count:
+                        count.append(nums[i] - i)
+                break
             if nums[i] - i not in diff:
                 diff[nums[i] - i] = 1
             else:
                 diff[nums[i] - i] += 1
                 if nums[i] -i not in count:
                     count.append(nums[i] - i)
+            if nums[j] - j not in diff:
+                diff[nums[j] - j] = 1
+            else:
+                diff[nums[j] - j] += 1
+                if nums[j] -j not in count:
+                    count.append(nums[j] - j)
+            i+=1
+            j-=1
+
+
+        # for i in range(len(nums)):
+        #     if nums[i] - i not in diff:
+        #         diff[nums[i] - i] = 1
+        #     else:
+        #         diff[nums[i] - i] += 1
+        #         if nums[i] -i not in count:
+        #             count.append(nums[i] - i)
 
         totalPairs = math.comb(len(nums), 2)
         for j in count:
