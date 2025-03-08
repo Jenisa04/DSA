2b0443516c812c80e4b132eb8c145f01691b23fa
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-03-08 03:16:57 -0800

    Time: 8 ms (7.74%) | Memory: 17.7 MB (62.29%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -9,7 +9,7 @@ class Solution:
                 if blocks[i] == 'W':
                     count +=1
         else:
-            minCtr = float('inf')
+            minCtr = 101
             for i in range(n-k+1):
                 blocksCtr = Counter(blocks[i:i+k])
                 if blocksCtr['W'] < minCtr:
