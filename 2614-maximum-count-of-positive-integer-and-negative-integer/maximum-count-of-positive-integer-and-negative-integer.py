2478875110ed40f94bae787795499c04e683c69c
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-03-12 16:40:45 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (92.47%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,29 +1,35 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
+        # edge cases:
+        # 1. if nums[0] is positive: only positive integers
+        # 2. if nums[last element] is negetave: only negative integers
+        # 3. if none if the above: can use binary search
+        n = len(nums)
+        if nums[0] > 0 or nums[n-1] < 0:
+            return n
+         
         l = 0
-        r = len(nums) - 1
-        pos = 0
-        neg = 0
-        if len(nums) == 1:
-            if nums[0] != 0:
-                return 1
-
+        r = n-1
+        target = 0
+        # find first positive integer
         while l<=r:
-            if nums[l] < 0:
-                neg+=1
-            elif nums[l] > 0:
-                pos+=1
-            if nums[r] < 0:
-                neg+=1
-            elif nums[r] > 0:
-                pos+=1
-            
-            if l==r:
-                if nums[l] < 0:
-                    neg-=1
-                elif nums[l] > 0:
-                    pos-=1
+            m = l + (r-l) // 2
+            if target >= nums[m]:
+                l = m+1
+            elif target < nums[m]:
+                r = m-1
         
-            l+=1
-            r-=1
+        pos = len(nums[l:])
+        
+        l = 0
+        r = n-1
+        target = 0
+        # find first negative integer
+        while l<=r:
+            m = l + (r-l) // 2
+            if target > nums[m]:
+                l = m+1
+            elif target <= nums[m]:
+                r = m-1
+        neg = len(nums[:r+1])
         return max(pos,neg)  
