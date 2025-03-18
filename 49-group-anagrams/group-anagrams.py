bd0276997f9e2074ec9484debd5052750c370519
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-03-18 12:09:44 -0700

    Time: 11 ms (83.59%) | Memory: 20.5 MB (95.68%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,20 +1,15 @@
+from collections import Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        if len(strs) <= 1:
+        if len(strs) == 1:
             return [strs]
-        anagrams = {}
+        allAnagrams = {}
+
+        for term in strs:
+            sortedVal = "".join(sorted(term))
+            if sortedVal in allAnagrams:
+                allAnagrams[sortedVal].append(term)
+            else:
+                allAnagrams[sortedVal] = [term]
         
-        for s in strs:
-            # Use the sorted string as a key
-            key = ''.join(sorted(s))
-            
-            # If the key is not already in the dictionary, add it
-            if key not in anagrams:
-                anagrams[key] = []
-            
-            # Append the current string to the list corresponding to the key
-            anagrams[key].append(s)
-        
-        # Return the grouped anagrams as a list
-        return list(anagrams.values())
+        return(list(allAnagrams.values()))
