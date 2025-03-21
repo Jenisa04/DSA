8abfda2ea959b8f3dbe75a5f9cafe9eb7a424642
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2025-03-21 03:32:57 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (68.88%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,35 +1,26 @@
+from collections import deque
 from typing import List
 
 class Solution:
-    def findNeighbors(self, r, c, m, n) -> List[List[int]]:
-        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        validNeighbors = []
-        for dr, dc in directions:
-            if self.isValid(r+dr, c+dc, m, n):
-                validNeighbors.append([r+dr, c+dc])
-        return validNeighbors
-            
-    def isValid(self, r, c, m, n) -> bool:
-        return 0 <= r < m and 0 <= c < n
-
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         m, n = len(image), len(image[0])
         original_color = image[sr][sc]
         
+        # Early exit if the starting color is the same as the target color
         if original_color == color:
             return image
 
-        queue = [[sr, sc]]
-        visited = set()
+        # Use a deque for optimized pop operations
+        queue = deque([(sr, sc)])
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         
         while queue:
-            r, c = queue.pop(0)
-            if (r, c) not in visited:
+            r, c = queue.popleft()
+            if image[r][c] == original_color:
                 image[r][c] = color
-                visited.add((r, c))
-                
-                for nr, nc in self.findNeighbors(r, c, m, n):
-                    if (nr, nc) not in visited and image[nr][nc] == original_color:
-                        queue.append([nr, nc])
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < m and 0 <= nc < n and image[nr][nc] == original_color:
+                        queue.append((nr, nc))
                         
         return image
